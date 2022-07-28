95252b4d2c444fae3c28bbc87f839ebeb1ed9821
Labeeb opc <Labeebopc313@gmail.com>
2022-07-28 11:37:23 +0530

    created components-header,login, cart,guest & created routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from './components/login/login.component'
+import {GuestComponent} from './components/guest/guest.component'
+import {CartComponent} from './components/cart/cart.component'
 
-const routes: Routes = [];
+
+// Created routes to the components
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'guest', component: GuestComponent},
+  {path: 'cart-page', component: CartComponent},
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/partials/header/header.component';
+import { LoginComponent } from './components/login/login.component';
+import { GuestComponent } from './components/guest/guest.component';
+import { CartComponent } from './components/cart/cart.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    GuestComponent,
+    CartComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
